25764c6d40fb7d13c2061d765338a79e88485fd5
ng881b0 <adib.bin-ali.external@airbus.com>
2021-02-19 17:56:10 +0100

    Level 3.1 complete

--- 3.1.fuel-injection-perfection/solution.py
+++ 3.1.fuel-injection-perfection/solution.py
@@ -1,1 +1,15 @@
 def solution(n):
+    n = int(n)
+    i = 0
+    while(n != 1):
+        if n & 1:
+            if ((n + 1) & 0b11 == 0) and n > 3:
+                n += 1
+                i += 1
+            else:
+                n -= 1
+                i += 1
+        else:
+            n = n >> 1
+            i += 1
+    return i
