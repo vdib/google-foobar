a29272e0140c4296991a2e3b273e8a7af2aa79b9
ng881b0 <adib.bin-ali.external@airbus.com>
2021-03-04 11:46:17 +0100

    Challenge 3.2 solved

--- 3.2.the-grandest-staircase-of-them-all/solution.py
+++ 3.2.the-grandest-staircase-of-them-all/solution.py
@@ -1,2 +1,20 @@
-def​ ​solution(n):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+# import math
+
+
+def solution(n):
+
+    s = [[0] * (n+1) for i in range(n+1)]
+
+    s[0][0] = 1
+
+    for total in range(1, n+1):
+        for h in range(n+1):
+            s[total][h] = s[total-1][h]
+
+            if h >= total:
+                s[total][h] += s[total - 1][h - total]
+
+    return s[n][n] - 1
+
+
+print(solution(9))
